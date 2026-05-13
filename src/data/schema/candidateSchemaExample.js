74696bafd1628b99a9f4fc55059d32d54c5e0a76
Rodrigo Ribeiro Leite <85045494+drigorleite@users.noreply.github.com>
2026-05-13 04:20:19 +0000

    Add investigative candidate schema structure

---
+++ src/data/schema/candidateSchemaExample.js
@@ -0,0 +1,44 @@
+export const candidateSchemaExample = {
+  id: 1,
+  name: 'Nome do Candidato',
+  slug: 'nome-do-candidato',
+
+  role: '',
+  party: '',
+  image: '',
+
+  executiveSummary: {
+    short: '',
+    long: '',
+  },
+
+  evidenceProfile: {
+    convictions: 0,
+    investigations: 0,
+    allegations: 0,
+    documentedEvents: 0,
+  },
+
+  investigations: [],
+  convictions: [],
+  acquittals: [],
+  controversies: [],
+  contradictions: [],
+
+  financialPatterns: [],
+
+  relatedPeople: [],
+  relatedCompanies: [],
+  relatedOrganizations: [],
+  relatedCases: [],
+
+  timeline: [],
+
+  mediaCoverage: [],
+  sourceLinks: [],
+
+  defenseArguments: [],
+  criticismArguments: [],
+
+  tags: [],
+};
