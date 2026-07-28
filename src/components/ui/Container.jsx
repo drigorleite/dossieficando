6526e5b5f6f8d5a8fe03b1b20dc0a45be9215f45
Rodrigo Ribeiro Leite <85045494+drigorleite@users.noreply.github.com>
2026-07-28 15:59:19 -0400

    Refine responsive container gutters

--- src/components/ui/Container.jsx
+++ src/components/ui/Container.jsx
@@ -2,7 +2,12 @@ import { cn } from '../../utils/cn';
 
 export default function Container({ children, className = '', as: Component = 'div' }) {
   return (
-    <Component className={cn('mx-auto max-w-7xl px-4 sm:px-6 lg:px-8', className)}>
+    <Component
+      className={cn(
+        'mx-auto w-full max-w-7xl px-4 min-[390px]:px-5 sm:px-6 lg:px-8',
+        className
+      )}
+    >
       {children}
     </Component>
   );
